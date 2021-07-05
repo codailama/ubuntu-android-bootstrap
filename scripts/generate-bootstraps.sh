a38fca58e4f818c4edbf804d984eb359c2b4ac09
Santosh Pingale <pingalesantosh@gmail.com>
2021-07-05 15:10:55 +0200

    Add gotty to the bootstrap

--- scripts/generate-bootstraps.sh
+++ scripts/generate-bootstraps.sh
@@ -27,21 +27,25 @@ build_bootstrap () {
 		PROOT_ARCH="aarch64"
 		ANDROID_ARCH="arm64-v8a"
 		MUSL_ARCH="aarch64-linux-musl"
+		GO_ARCH="arm64"
 		;;
 	armhf)
 		PROOT_ARCH="armv7a"
 		ANDROID_ARCH="armeabi-v7a"
 		MUSL_ARCH="arm-linux-musleabihf"
+		GO_ARCH="arm"
 		;;
 	amd64)
 		PROOT_ARCH="x86_64"
 		ANDROID_ARCH="x86_64"
 		MUSL_ARCH="x86_64-linux-musl"
+		GO_ARCH="amd64"
 		;;
 	i386)
 		PROOT_ARCH="i686"
 		ANDROID_ARCH="x86"
 		MUSL_ARCH="i686-linux-musl"
+		GO_ARCH="386"
 		;;
 	*)
 		echo "Invalid arch"
@@ -59,11 +63,13 @@ build_bootstrap () {
   rm -rf rootfs.tar.gz
   curl -o rootfs.tar.gz -L "http://cdimage.ubuntu.com/ubuntu-base/releases/20.04/release/ubuntu-base-20.04.2-base-$1.tar.gz"
 
+  curl -o gotty.tar.gz -L "https://github.com/sorenisanerd/gotty/releases/download/v1.3.0/gotty_v1.3.0_linux_$GO_ARCH.tar.gz"
+
 	cp ../../run-bootstrap.sh .
 	cp ../../install-bootstrap.sh .
 	cp ../../add-user.sh .
 	cp ../../ioctlHook.c .
-	zip -r bootstrap-$PROOT_ARCH.zip root ioctlHook.c root-pre5 rootfs.tar.gz run-bootstrap.sh install-bootstrap.sh add-user.sh
+	zip -r bootstrap-$PROOT_ARCH.zip root ioctlHook.c root-pre5 rootfs.tar.gz run-bootstrap.sh install-bootstrap.sh add-user.sh gotty.tar.gz
 	mv bootstrap-$PROOT_ARCH.zip ../
 	echo "Packed bootstrap $1"
 	cd ..
--- scripts/initialize-bootstrap.sh
+++ scripts/initialize-bootstrap.sh
@@ -3,11 +3,7 @@
 export DEBIAN_FRONTEND=noninteractive
 export DEBCONF_NONINTERACTIVE_SEEN=true
 apt update
-apt-get install --no-install-recommends -y openssh-server \
-  openssh-client \
-  gcc-aarch64-linux-gnu \
-  build-essential \
-  vim \
+apt-get install --no-install-recommends -y vim \
   ffmpeg \
   curl \
   git \
@@ -21,3 +17,8 @@ echo \"PermitRootLogin yes\" >> /etc/ssh/sshd_config
 ssh-keygen -A
 mkdir -p /var/run/sshd
 chmod 700 /var/run/sshd
+
+# Generate ioctl.so
+gcc -fPIC -c -o ioctlHook.o /root/ioctlHook.c
+gcc -shared -o ioctlHook.so ioctlHook.o -ldl
+cp ioctlHook.so /home/octoprint/ioctlHook.so
